f292fd3006b278f569ff65156380803ce5c260dc
himanchal08 <khattrihimanchal17@gmail.com>
2024-08-06 00:15:44 +0530

    added expansion

--- script.js
+++ script.js
@@ -3,22 +3,12 @@ const back = document.querySelector("body");
 const ball = document.querySelector(".toggle_ball");
 const label = document.querySelector(".toggle_label");
 const save = localStorage.getItem("theme");
+const darkMode = document.querySelector(".dark-mode")
+
 
 
 btn.addEventListener("click", () => {
+    darkMode.classList.toggle('active');
     
-  if (back.classList.contains("dark_mode")) {
-    back.classList.remove("dark_mode");
-    back.classList.add("light_mode");
-    label.classList.remove("light_mode");
-    ball.classList.add("light_mode");
-    label.classList.add("dark_mode");
-    
-  } else {
-    back.classList.remove("light_mode");
-    back.classList.add("dark_mode");
-    ball.classList.add("dark_mode");
-    label.classList.add("light_mode");
-    
-  }
+
 });
